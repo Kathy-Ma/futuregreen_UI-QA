edfc57cf13da5e426c00b5b95a2de1d51450777d
yichen236 <yichenli236@gmail.com>
2026-03-21 14:10:36 -0400

    added feedback functionality, scollable previous images, designed assets

--- trashcnn/app/(tabs)/action.tsx
+++ trashcnn/app/(tabs)/action.tsx
@@ -1,11 +1,26 @@
-/*lmao i have no idea how this code works*/
 import { useState } from 'react';
-import { Alert, Button, Image, Text, View, StyleSheet, TouchableOpacity, useColorScheme} from 'react-native';
-import { predictImage } from '@/services/api';
+import { Alert, Button, Image, Text, View, StyleSheet, TouchableOpacity, Modal, TextInput, ImageBackground, ScrollView} from 'react-native';
+import { predictImage, submitReview } from '@/services/api';
 import * as ImagePicker from 'expo-image-picker';
 export default function ImagePickerExample() {
+  const [previousImages, setPreviousImages] = useState<
+  { uri: string; prediction: string | null; confidence: number | null }[]
+>([]);
   const [image, setImage] = useState<string | null>(null);
+  const [reviewVisible, setReviewVisible] = useState(false);
+  const openReview = () => {
+  setReviewVisible(true);
+};
+
+const closeReview = () => {
+  setReviewVisible(false);
+};
   const [message, setMessage] = useState<string | null>(null);
+  const [starState, setStarState] = useState(0);
+  const handleStarPress = (rating: number) => {
+  setStarState(rating);
+};
+  const [reviewText, setReviewText] = useState('');
   const [predictionResult, setPredictionResult] = useState<string | null>(null);
   const [confidence, setConfidence] = useState<number | null>(null);
   const takePhoto = async () => {
@@ -45,6 +60,14 @@ export default function ImagePickerExample() {
     setMessage(msg);
     setPredictionResult(result.prediction_result);
     setConfidence(result.confidence);
+    setPreviousImages(prev => [
+  ...prev,
+  {
+    uri: asset.uri,
+    prediction: result.prediction_result,
+    confidence: result.confidence,
+  },
+]);
   } catch (error) {
     console.error('Error predicting image:', error);
   }
@@ -69,7 +92,6 @@ export default function ImagePickerExample() {
     });
 
     console.log(result);
-/* i dont know hwat api using*/
 
     if (!result.canceled && result.assets?.length > 0) {
   const asset = result.assets[0];
@@ -94,32 +116,112 @@ export default function ImagePickerExample() {
     setMessage(msg);
     setPredictionResult(result.prediction_result);
     setConfidence(result.confidence);
+    setPreviousImages(prev => [
+  ...prev,
+  {
+    uri: asset.uri,
+    prediction: result.prediction_result,
+    confidence: result.confidence,
+  },
+]);
   } catch (error) {
     console.error('Error predicting image:', error);
   }
 }
   };
   return (
-    
-    <View style={styles.container}>
+    <ImageBackground
+    source={require('@/assets/UIQAimages/background.png')}
+    style={styles.container}
+    resizeMode="cover"
+    >
+      <ScrollView contentContainerStyle={{ alignItems: 'center', paddingBottom: 50 }}>
+        <Modal
+  animationType="fade"
+  transparent={true}
+  visible={reviewVisible}
+  onRequestClose={closeReview}
+>
+  <View style={styles.modalOverlay}>
+    <ImageBackground
+  source={require('@/assets/UIQAimages/popupBackground.png')}
+  style={styles.modalContent}
+  resizeMode="cover"
+>
+      <Text style={styles.title2}>How'd we do?</Text>
+<View style={{ flexDirection: "row", justifyContent: "center", marginTop:20, marginBottom:20,}}>
+  {[1, 2, 3, 4, 5].map((star) => (
+    <TouchableOpacity key={star} onPress={() => handleStarPress(star)}>
+      <Image
+        source={
+          starState >= star
+            ? require('@/assets/UIQAimages/fullstar.png')
+            : require('@/assets/UIQAimages/emptystar.png')
+        }
+        resizeMode="contain"
+        style={{ width: 40, height: 40, marginHorizontal: 5 }}
+      />
+    </TouchableOpacity>
+  ))}
+</View>
+  <TextInput
+  style={styles.subtitle}
+  placeholder="Write your feedback..."
+  placeholderTextColor="#ccc"
+  value={reviewText}
+  onChangeText={setReviewText}
+  multiline
+/>
+<TouchableOpacity style={styles.imageBubble2} onPress={() => submitReview(starState,reviewText)}>
+      <Text style={styles.bubbleText}>Submit Review</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.imageBubble2} onPress={closeReview}>
+      <Text style={styles.bubbleText}>close</Text>
+      </TouchableOpacity>
+      </ImageBackground>
+    </View>
+</Modal>
        <Text style={styles.title}>Future Fusion AI</Text>
 
-  <TouchableOpacity style={{backgroundColor: 'lightblue', padding: 6, borderRadius: 11}} onPress={pickImage}>
-    <Text style={{fontSize: 18, fontWeight: "600"}}>Upload from Camera Roll</Text>
+  <TouchableOpacity style={styles.imageBubble} onPress={pickImage}>
+    <Text style={styles.bubbleText}>Upload from Camera Roll</Text>
   </TouchableOpacity>
 
     <View style={{ height: 20 }} />
 
-  <TouchableOpacity style={{backgroundColor: 'lightblue', padding: 6, borderRadius: 11}} onPress={takePhoto}>
-    <Text style={{fontSize: 18, fontWeight: "600"}}>Take Photo</Text>
+  <TouchableOpacity style={styles.imageBubble} onPress={takePhoto}>
+    <Text style={styles.bubbleText}>Take Photo</Text>
   </TouchableOpacity>
 
   {image && <Image source={{ uri: image }} style={styles.image} />}
-
   {image && <Text style={styles.resultTitle}>Your garbage is {predictionResult || '(analyzing...)'}</Text>}
   {predictionResult && confidence && <Text style={styles.subtitle}>Confidence: {(confidence * 100).toFixed(2)}%</Text>}
   {message && <Text style={styles.subtitle}>{message}</Text>}
-</View>
+  {previousImages.length != 0 ? (
+ <TouchableOpacity style={styles.imageBubble} onPress={openReview}>
+  <Text style={styles.bubbleText}>How'd we do?</Text>
+</TouchableOpacity>
+  ):null}
+  <Text style={styles.title2}>Previous Images</Text>
+  <ScrollView horizontal style={{ marginTop: 20 }}>
+  {previousImages.length == 0 ? (
+    <Text style={styles.subtitle}>Nothing here yet</Text>
+  ) : (
+    previousImages.map((item, index) => (
+      <View key={index} style={{ marginRight: 10, alignItems: 'center' }}>
+        <Image source={{ uri: item.uri }} style={{ width: 100, height: 150 }} />
+        <Text style={{ color: '#FFF', fontSize: 12 }}>{item.prediction}</Text>
+        {item.confidence !== null && (
+          <Text style={{ color: '#FFF', fontSize: 12 }}>
+            {(item.confidence * 100).toFixed(2)}%
+          </Text>
+        )}
+      </View>
+    ))
+  )}
+</ScrollView>
+  </ScrollView>
+</ImageBackground>
 
   );
 }
@@ -128,7 +230,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#132908',
     alignItems: 'center',
-    /*this means everythings starts at the top lmao*/
     justifyContent: 'flex-start',
     paddingTop: 100,
   },
@@ -137,6 +238,12 @@ const styles = StyleSheet.create({
     color: '#FFFF',
     fontWeight:'bold',
     marginBottom: 60,
+  },
+  title2:{
+    fontSize:30,
+    color: '#FFFF',
+    fontWeight:'bold',
+    marginTop: 20,
   },
   resultTitle:{
     fontSize:30,
@@ -149,6 +256,7 @@ const styles = StyleSheet.create({
     color: '#FFFF',
     fontWeight: 'normal',
     marginTop: 5,
+    marginBottom:10,
     textAlign: 'center',
   },
   image: {
@@ -157,8 +265,37 @@ const styles = StyleSheet.create({
     marginTop:15,
     marginBottom:15,
   },
+  imageBubble:{
+    backgroundColor:'#4ca626',
+    padding: 6,
+    borderRadius: 11,
+  },
+  bubbleText:{
+    color:'#FFFF',
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  imageBubble2:{
+    backgroundColor:'#4ca626',
+    padding:6,
+    borderRadius: 11,
+    marginBottom:20,
+  },
+ modalOverlay: {
+  flex: 1,
+  backgroundColor: 'rgba(0,0,0,0.5)',
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+
+modalContent: {
+  width: 300,
+  borderRadius: 10,
+  padding: 20,
+  alignItems: 'center',
+  overflow: 'hidden',
+},
 });
-//lowk the model is wrong so much but whatever thats not my problem
 function binCheck (trashtype: string){
   let message = null
   if (trashtype=="metal"){
@@ -182,6 +319,5 @@ function binCheck (trashtype: string){
   else if (trashtype=="organic"){
     message = "This goes in the organics (green) bin, make sure all packaging is removed"
   }
-  console.log(message)
   return message
 }
